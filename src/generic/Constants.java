688d02b1350b00a6c669ca11db2e466ee354efa8
Gurupreeth Singh <igurupreeth@gmail.com>
2023-04-11 04:35:15 +0530

    token and url of the github repository added.

--- src/generic/Constants.java
+++ src/generic/Constants.java
@@ -5,4 +5,6 @@ public interface Constants {
 	  String excelsheetaddress = "C:\\Users\\Administrator\\Desktop\\latest_Eclipse\\all_programs\\frameworkdesign\\driverexecutalbes\\mydata.xlsx";
 	  String chromekey = "webdriver.chrome.driver";
 	  String chromevalue = "C:\\Users\\Administrator\\Desktop\\latest_Eclipse\\all_programs\\frameworkdesign\\driverexecutalbes\\chromedriver.exe";
+	 // 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
+	  //https://github.com/gurupreethsingh/ecoders_selenium_automation.git
 }
